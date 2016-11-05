36f9e9948dcc6a788c4069b6f738935a06c5a302
abhi.misc244 <abhi.mic244@gmail.com>
2016-11-05 18:15:29 +0800

    test

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ from graphnode import GraphNode
 from graphedge import GraphEdge
 from graphinterface import GraphInterface
 from kivy.uix.button import Button
+from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, NumericProperty, ReferenceListProperty, ObjectProperty
 
 class MainScreen(Screen):
-    mat = ObjectProperty(None)
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
         box = BoxLayout(orientation='vertical')
@@ -22,72 +22,63 @@ class MainScreen(Screen):
         self.game = GraphInterface()
         box.add_widget(self.game)
 
+        box2 = BoxLayout(orientation='horizontal')
+
         button = Button (text = 'To Calculating Screen', size_hint=(.2,.2))
         button.bind(on_press=self.switch_screen)
-        box.add_widget(button)
+        box2.add_widget(button)
 
         node_button = Button (text = 'Create Node', size_hint=(.2,.2))
         node_button.bind(on_press=self.game.createNode)
-        box.add_widget(node_button)
+        box2.add_widget(node_button)
 
         edge_button = Button (text = 'Create Edge', size_hint=(.2,.2))
         edge_button.bind(on_press=self.game.createEdge)
-        box.add_widget(edge_button)
+        box2.add_widget(edge_button)
+
+        box.add_widget(box2)
         self.add_widget(box)
+
 
     def switch_screen(self, instance):
+        global mat
         self.parent.current = 'calculating_screen'
         mat = self.game.graph
+        print 'this is mat---->>>', mat
 
 class AnotherScreen(Screen):
-    b = ObjectProperty(None)
     def __init__(self, **kwargs):
         super(AnotherScreen, self).__init__(**kwargs)
-        box = BoxLayout()
+        global mat
+
+        self.box = BoxLayout(orientation = 'vertical')
         label = Label(text='Calculating Screen')
-        box.add_widget(label)
-        self.add_widget(box)
+        self.box.add_widget(label)
+
+        self.add_widget(self.box)
+
+        box2=BoxLayout(orientation='horizontal')
 
         button = Button (text = 'To Main Screen', size_hint=(.2,.2))
         button.bind(on_press=self.switch_screen)
-        self.add_widget(button)
+        box2.add_widget(button)
 
-
+        self.box.add_widget(box2)
 
     def switch_screen(self, instance):
         self.parent.current = 'drawing_screen'
-        print self.b
-        #self.b = self.parent.mat
-
 
 class ScreenManagement(ScreenManager):
     pass
 
-''''def updategraph(object):
-    print '-------------'
-    presentation.screens[1].b = presentation.screens[0].mat
-''''
-presentation = ScreenManager(transition=FadeTransition())
-drawing_screen = MainScreen(name='drawing_screen')
-calculating_screen = AnotherScreen(name = 'calculating_screen')
-presentation.add_widget(drawing_screen)
-presentation.add_widget(calculating_screen)
-
-''''print presentation.screens[0].mat
-print presentation.screens[1].b
-presentation.screens[0].bind(mat = updategraph)
-print presentation.screens[0].mat
-print presentation.screens[1].b
-''''
-
-'''List all variable shared bewteen screen here.
-Create ObjectProperty instances in both screens.
-Use "bind" and "setter" function to link two variables in different screens'''
-
 class MainApp(App):
     def build(self):
-        #presentation.current = 'drawing_screen'
-        #presentation.Screen(calculating_screen)
+        presentation = ScreenManager(transition=FadeTransition())
+        drawing_screen = MainScreen(name='drawing_screen')
+        calculating_screen = AnotherScreen(name = 'calculating_screen')
+        presentation.add_widget(drawing_screen)
+        presentation.add_widget(calculating_screen)
+        #presentation.screens[0].bind(mat=presentation.screens[0].setter('b'))
         return presentation
 
 if __name__ == "__main__":
